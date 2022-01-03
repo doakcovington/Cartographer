ef57795da8ed3442f99a8770cefaa8fb391437c4
Doak <doakcovington@protonmail.com>
2022-01-02 19:21:41 -0800

    Built out main menu

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
 // WDGTR
 // NCWH
 // Christmas 2021
+use std::io;
+use std::process;
 
 fn main() {
-    println!("Howdy, world!");
+    menu()
 }
+
+fn menu() {
+    println!("CARTOGRAPHER");
+    println!("> 1. New Game");
+    println!("> 2. Exit");
+    println!("> Please make a selection:");
+
+    let mut selection = String::new();
+
+    io::stdin()
+        .read_line(&mut selection)
+        .expect("Failed to process selection");
+    println!("You guessed: {}", selection);
+    let input: u32 = selection
+        .trim()
+        .parse()
+        .expect("Wanted 1 or 2");
+    if input == 1 {
+        start_game();
+    } else {
+        process::exit(1);
+    }
+}
+
+fn start_game() {
+    println!("Howdy from start game")
+}
+
+
